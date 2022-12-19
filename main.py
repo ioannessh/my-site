ab3ce9ecb5f73f39330ffa6873db897e9dd6266e
Ivan Shurenkov <ivan.shurenkov@yandex.ru>
2022-12-19 23:34:55 +0000

    Disable debug

--- main.py
+++ main.py
@@ -52,4 +52,4 @@ if __name__ == '__main__':
                 db_init()
                 sys.exit(0)
 
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
